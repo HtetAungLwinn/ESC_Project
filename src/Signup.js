869f372be7b8beb1041eb63f81e20b500c375e71
HtetAungLwinn <150445388+HtetAungLwinn@users.noreply.github.com>
2025-07-08 15:41:17 +0800

    sign up page

--- src/Signup.js
+++ src/Signup.js
@@ -2,19 +2,52 @@ import React from "react";
 
 export default function Signup() {
   return (
-    <div className="auth-page">
-      <h2>Sign Up</h2>
+  
+    
+    <div className="centered">
+      <h2 style={{ padding:"50px"}}>Sign Up</h2>
       <form>
-        <label htmlFor="email">Email</label>
+        <h3 style={{ backgroundColor: "#dbdbdb", paddingLeft:"100px", textAlign: "left"}}>1. Identification details</h3>
+        <div className="flex-container">
+        <div className="input-group">
+        <label htmlFor="salutation">Salutation:</label>
+        <input type="salutation" id="salutation" name="salutation" />
+        </div>
+        <div className="input-group">
+        <label htmlFor="fname">First name:</label>
+        <input type="fname" id="fname" name="fname" />
+        </div>
+        <div className="input-group">
+        <label htmlFor="lname">Last name:</label>
+        <input type="lname" id="lname" name="lname" />
+        </div>
+        </div>
+        
+        <h3 style={{ backgroundColor: "#dbdbdb", paddingLeft:"100px", textAlign: "left"}}>2. Contact details</h3>
+        <div className="flex-container">
+        <div className="input-group">
+        <label htmlFor="contact">Contact:</label>
+        <input type="contact" id="contact" name="contact" />
+        </div>
+        </div>
+
+
+        <h3 style={{ backgroundColor: "#dbdbdb", paddingLeft:"100px", textAlign: "left" }}>3. Login details</h3>
+        <div className="flex-container">
+        <div className="input-group">
+        <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" />
-
-        <label htmlFor="password">Password</label>
+        </div>
+        <div className="input-group">
+        <label htmlFor="password">Password:</label>
         <input type="password" id="password" name="password" />
-
-        <label htmlFor="confirm">Confirm Password</label>
+        </div>
+        <div className="input-group">
+        <label htmlFor="confirm">Confirm Password:</label>
         <input type="password" id="confirm" name="confirm" />
-
-        <button type="submit">Create Account</button>
+        </div>
+        </div>
+        <h3><button type="submit">Create Account</button></h3>
       </form>
     </div>
   );
