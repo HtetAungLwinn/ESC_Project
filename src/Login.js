e9b04cc4f822075bfee25bb48c038edacf3ce765
HtetAungLwinn <150445388+HtetAungLwinn@users.noreply.github.com>
2025-07-08 15:56:28 +0800

    login up page

--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,22 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="auth-page">
-      <h2>Login</h2>
+    <div className="centered">
+      <h2 style={{ padding: "50px" }}>Login</h2>
       <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-        
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-        
-        <button type="submit">Log In</button>
+        <div className="login-form-container">
+          <div className="input-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+        </div>
+        <div className="submit-btn-container">
+          <button type="submit">Log In</button>
+        </div>
       </form>
     </div>
   );
