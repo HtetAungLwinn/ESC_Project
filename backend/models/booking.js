bd493803aaaa5736acd46ddb07f59b6898505e32
Balls99 <88177640+FT16@users.noreply.github.com>
2025-07-30 14:41:08 +0800

    added getAllBookings to bookings model

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -38,24 +38,48 @@ async function createBooking(req, res){
 }
 
 async function getBookingByID(req,res){
-    const { id } = req.params;
+    const { id, uid } = req.params;
 
     try{
-        const [rows] = await db.query('SELECT * FROM bookings WHERE bid = ?', [id]);
+        const [rows] = await db.query('SELECT * FROM bookings WHERE bid = ? AND uid = ?', [id, uid]);
 
         if (rows.length === 0) {
             return res.status(404).json({ error: 'Booking not found' });
         }
 
         const booking = rows[0];
-        booking.stay_info = booking.stay_info;
-        booking.payment_info = booking.payment_info;
+        booking.stay_info = JSON.parse(booking.stay_info);
+        booking.payment_info = JSON.parse(booking.payment_info);
 
         res.status(200).json(booking);
     } catch (err) {
         console.error('Fetch error:', err);
         res.status(500).json({ error: 'Failed to retrieve booking' });
     }
+}
+
+async function getAllBookings(req,res) {
+  const { uid } = req.params;
+
+  try{
+    const [rows] = await db.query('SELECT * FROM bookings WHERE uid = ?', [uid]);
+
+    if (rows.length == 0){
+      return res.status(404).json({ error: 'No bookings found' });
+    }
+
+    const bookings = rows;
+    for (let i in bookings){
+      bookings[i].stay_info = JSON.parse(bookings[i].stay_info);
+      bookings[i].payment_info = JSON.parse(bookings[i].payment_info);
+    }
+
+    res.status(202).json(bookings)
+  }catch (err) {
+    console.error('Fetch error');
+    res.status(500).json({ error: 'Failed to recieve bookings' });
+  }
+  
 }
 
 module.exports = {
---
+++ src/BookingDetailsPage.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
+export default function BookingDetailsPage(){
+    const [bookings, setBookings] = useState([]);
+}
