956f84fc76c11fe270475ca96cf767c885b3bbd7
STHSCare12 <selina.sths@gmail.com>
2025-08-13 11:27:45 +0800

    Solve vercel connection

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,22 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000' })); 
+const allowedOrigins = [
+  'http://localhost:3000', 'https://escproject.vercel.app/'];
+
+// app.use(cors({ origin: 'http://localhost:3000' })); 
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin (like Postman or curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 
 // Routes
